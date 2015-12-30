1b9cc63d53e4e54c5e51c736d9cc8fe0c6146808
xuzhiqiang <xuzhiqiang@chelun.com>
2015-12-30 13:38:04 +0000

    refactor Server

--- ideas/procpp/event_manager/Server.cpp
+++ ideas/procpp/event_manager/Server.cpp
@@ -12,22 +12,49 @@ static void error_exit(const char *s)
 	exit(1);
 }
 
+static struct sockaddr_in addr4(uint16_t port)
+{
+	struct sockaddr_in serveraddr;
+	bzero(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(port);
 
-bool Server::listen(uint16_t port, EventManager::EventCB callbacks, int domain, const struct sockaddr *addr, socklen_t addrlen)
+	return serveraddr;
+}
+
+static struct sockaddr_in6 addr6(uint16_t port)
+{
+	struct sockaddr_in6 serveraddr;
+	bzero(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin6_family = AF_INET6;
+	serveraddr.sin6_port = htons(port);
+	serveraddr.sin6_addr = in6addr_any;
+
+	return serveraddr;
+}
+
+bool Server::listen(uint16_t port, EventManager::EventCB callbacks, int domain)
 {
 	switch (domain) {
 		case AF_INET:
-			return listen4(port, callbacks, addr, addrlen);
+		{
+			auto serveraddr = addr4(port);
+			return listen4(&serveraddr, callbacks);
 			break;
+		}
 		case AF_INET6:
-			return listen6(port, callbacks, addr, addrlen);
+		{
+			auto serveraddr = addr6(port);
+			return listen6(&serveraddr, callbacks);
 			break;
+		}
 	}
 
 	return false;
 }
 
-bool Server::listen4(uint16_t port, EventManager::EventCB callbacks, const struct sockaddr *addr, socklen_t addrlen)
+bool Server::listen4(const struct sockaddr_in *addr, EventManager::EventCB callbacks)
 {
 	auto s = socket(AF_INET, SOCK_STREAM, 0);
 	if (s == -1) error_exit("socket");
@@ -35,52 +62,35 @@ bool Server::listen4(uint16_t port, EventManager::EventCB callbacks, const struc
 	const int enable = 1;
 	if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) == -1) error_exit("setsockopt");
 
-	if (!addr) {
-		struct sockaddr_in serveraddr;
-		bzero(&serveraddr, sizeof(serveraddr));
-		serveraddr.sin_family = AF_INET;
-		serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-		serveraddr.sin_port = htons(port);
-
-		addr = (struct sockaddr *)&serveraddr;
-		addrlen = sizeof(serveraddr);
-	}
-
 	// bind,listen接受的都是sockaddr类型参数
-	if (bind(s, addr, addrlen) < 0) error_exit("bind");
+	if (bind(s, reinterpret_cast<const struct sockaddr*>(addr), sizeof(*addr)) < 0) error_exit("bind");
 
 	if (::listen(s, 100) < 0) error_exit("listen");
 
 	_m.watch(s, callbacks);
 
+	auto port = ntohs(addr->sin_port);
 	cout << "listening port " << port << endl;
 
 	return true;
 }
 
-bool Server::listen6(uint16_t port, EventManager::EventCB callbacks, const struct sockaddr *addr, socklen_t addrlen)
+bool Server::listen6(const struct sockaddr_in6 *addr, EventManager::EventCB callbacks)
 {
 	auto s = socket(AF_INET6, SOCK_STREAM, 0);
 	if (s == -1) error_exit("socket");
 
-	if (!addr) {
-		struct sockaddr_in6 serveraddr;
-		bzero(&serveraddr, sizeof(serveraddr));
-		serveraddr.sin6_family = AF_INET6;
-		serveraddr.sin6_port = htons(port);
-		serveraddr.sin6_addr = in6addr_any;
-
-		addr = (struct sockaddr *)&serveraddr;
-		addrlen = sizeof(serveraddr);
-	}
+	const int enable = 1;
+	if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) == -1) error_exit("setsockopt");
 
 	// bind,listen接受的都是sockaddr类型参数
-	if (bind(s, addr, addrlen) < 0) error_exit("bind");
+	if (bind(s, reinterpret_cast<const struct sockaddr*>(addr), sizeof(*addr)) < 0) error_exit("bind");
 
 	if (::listen(s, 100) < 0) error_exit("listen");
 
 	_m.watch(s, callbacks);
 
+	auto port = ntohs(addr->sin6_port);
 	cout << "listening port " << port << endl;
 
 	return true;
--- ideas/procpp/event_manager/Server.h
+++ ideas/procpp/event_manager/Server.h
@@ -6,22 +6,26 @@
 class IServer
 {
 public:
-	virtual bool listen(uint16_t port, EventManager::EventCB callbacks, int domain, const struct sockaddr *addr, socklen_t addrlen) = 0;
-	virtual bool listen_u(uint16_t port, EventManager::EventCB callbacks, int domain, const struct sockaddr *addr, socklen_t addrlen) = 0;
+	virtual bool listen(uint16_t port, EventManager::EventCB callbacks, int domain) = 0;
+	virtual bool listen(const struct sockaddr *addr, socklen_t addrlen, EventManager::EventCB callbacks) = 0;
+	virtual bool listen_u(uint16_t port, EventManager::EventCB callbacks, int domain) = 0;
+	virtual bool listen_u(const struct sockaddr *addr, socklen_t addrlen, EventManager::EventCB callbacks) = 0;
 	virtual void start() = 0;
 };
 
 class Server : public IServer
 {
 public:
-	virtual bool listen(uint16_t port, EventManager::EventCB callbacks, int domain = AF_INET, const struct sockaddr *addr = nullptr, socklen_t addrlen = 0) override;
-	virtual bool listen_u(uint16_t port, EventManager::EventCB callbacks, int domain = AF_INET, const struct sockaddr *addr = nullptr, socklen_t addrlen = 0) override { return false; };
+	virtual bool listen(uint16_t port, EventManager::EventCB callbacks, int domain = AF_INET) override;
+	virtual bool listen(const struct sockaddr *addr, socklen_t addrlen, EventManager::EventCB callbacks) override { return false; };
+	virtual bool listen_u(uint16_t port, EventManager::EventCB callbacks, int domain = AF_INET) override { return false; };
+	virtual bool listen_u(const struct sockaddr *addr, socklen_t addrlen, EventManager::EventCB callbacks) override { return false; };
 	virtual void start() override;
 	EventManager& getEventManager() { return _m; }
 
 private:
-	bool listen4(uint16_t port, EventManager::EventCB callbacks, const struct sockaddr *addr = nullptr, socklen_t addrlen = 0);
-	bool listen6(uint16_t port, EventManager::EventCB callbacks, const struct sockaddr *addr = nullptr, socklen_t addrlen = 0);
+	bool listen4(const struct sockaddr_in *addr, EventManager::EventCB callbacks);
+	bool listen6(const struct sockaddr_in6 *addr, EventManager::EventCB callbacks);
 	EventManager _m;
 
 	friend class Echo;
