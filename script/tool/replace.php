0c1fec967dfec4fdc484df272ae5d890c6297b73
xuzhiqiang <xuzhiqiang@chelun.com>
2016-01-26 06:56:45 +0000

    replace.php for replacement in multiple files

---
+++ script/tool/replace.php
@@ -0,0 +1,17 @@
+<?php
+include __DIR__ . "/common.php";
+
+
+get_options();
+
+
+$remaining_args = get_remaining_args();
+
+list($str1, $str2, $dir) = $remaining_args;
+
+var_dump($remaining_args);
+
+$run_cmd = "find $dir -type f -exec sed -i " . escapeshellarg("s/$str1/$str2/g") . " {} \\;";
+
+
+shell_exec_realtime_output($run_cmd);
