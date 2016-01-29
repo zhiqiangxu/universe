07148b5c467e9f926683758db1d3e348b103804b
xuzhiqiang <xuzhiqiang@chelun.com>
2016-01-29 08:06:30 +0000

    ucontext demo
    It's assentially like thread.
    So it's not very useful.(yet)

---
+++ ideas/procpp/event_manager/tests/ucontext.cpp
@@ -0,0 +1,14 @@
+#include <ucontext.h>  
+#include <unistd.h>  
+#include <iostream>
+using namespace std;
+  
+int main(int argc, const char *argv[]){  
+	ucontext_t context;  
+  
+	getcontext(&context);  
+	cout << "Hello world" << endl;  
+	sleep(1);  
+	setcontext(&context);  
+	return 0;  
+}  
