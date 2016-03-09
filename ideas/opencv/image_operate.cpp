6f420ef5b6808a9d114e6f0250a76923d166d328
xuzhiqiang <xuzhiqiang@chelun.com>
2016-03-09 02:51:38 +0000

    include

--- ideas/opencv/image_operate.cpp
+++ ideas/opencv/image_operate.cpp
@@ -1,6 +1,4 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
 #include <vector>
 #include <unistd.h>
 
