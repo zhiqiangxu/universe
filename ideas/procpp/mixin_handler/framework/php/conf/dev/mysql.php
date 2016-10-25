1c792fdb796d630898befe909e0f179b27a1c1dc
xuzhiqiang15 <652732310@qq.com>
2016-10-25 15:06:02 +0800

    fix

--- ideas/procpp/mixin_handler/framework/php/conf/dev/mysql.php
+++ ideas/procpp/mixin_handler/framework/php/conf/dev/mysql.php
@@ -1,15 +1,4 @@
 <?php
-/*
-$conf['default'] = [
-    'host'  => '10.209.44.12',
-    'port'  => 10043,
-    'dbname'=> 'test',
-    'user'  => 'admin_framework',
-    'psw'   => 'admin_framework',
-    'encode'=> 'utf8'
-];
-*/
-
 $conf['default'] = [
     'host'  => 'localhost',
     'port'  => 3306,
